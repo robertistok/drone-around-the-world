90660932fc74f6228230cffadfff308a8a40bad4
robertistok <robertistok94@gmail.com>
2019-05-17 22:19:15 +0200

    Create Loader.js

---
+++ client/src/components/Loader.js
@@ -0,0 +1,9 @@
+import React from "react";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import styled from "styled-components";
+
+const Loader = () => {
+  return <LinearProgress />;
+};
+
+export default Loader;
