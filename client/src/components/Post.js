1f44f5c7ca8fe270a1fe85adaa7c2d6d790db3fe
robertistok <robertistok94@gmail.com>
2019-05-18 20:06:47 +0200

    Fix cursor jumping around

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Fab from "@material-ui/core/Fab";
@@ -28,13 +28,19 @@ const UPDATE_POST_MUTATION = gql`
 
 const Post = ({ mediaPath, captionForFacebook, id, isPublished }) => {
   const updatePost = useMutation(UPDATE_POST_MUTATION);
+  const [inputState, setInputState] = useState({ captionForFacebook, isPublished });
 
   const handleInputChange = event => {
     event.persist();
 
+    const { name, value, checked } = event.target;
+    const newInputState = { [name]: value || checked };
+
     updatePost({
-      variables: { id, [event.target.name]: event.target.value || event.target.checked }
+      variables: { id, ...newInputState }
     });
+
+    setInputState({ ...inputState, ...newInputState });
   };
 
   return (
@@ -54,13 +60,17 @@ const Post = ({ mediaPath, captionForFacebook, id, isPublished }) => {
           multiline
           margin="normal"
           fullWidth
-          value={captionForFacebook}
+          value={inputState.captionForFacebook}
           onChange={handleInputChange}
         />
       </Grid>
 
       <Grid item xs={1}>
-        <Checkbox name="isPublished" checked={isPublished} onChange={handleInputChange} />
+        <Checkbox
+          name="isPublished"
+          checked={inputState.isPublished}
+          onChange={handleInputChange}
+        />
       </Grid>
     </Grid>
   );
